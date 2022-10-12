a89a8abeb40f148e66399c7a290dd385a192d8ea
SwapnilGite <gitesmait@gmail.com>
2022-10-12 12:29:50 +0530

    final

--- index.js
+++ index.js
@@ -2,9 +2,149 @@ const userName = document.getElementById("name");
 const submitBtn = document.getElementById("submitBtn");
 const map = new Map();
 // const notice = new Notice();
-map.set('gitesmait@gmail.com','Swapnil Gite');
-map.set('pratikpatil@gmail.com','Pratik Patil');
-map.set('ashwinigite007@gmail.com','Ashwini Gite');
+
+map.set('8947286','Abhishek Kulkarni');
+map.set('8947001','Gaurav Shelar');
+map.set('8946978','Kanad Phuke');
+map.set('8946970','PRATIK ROTHE');
+map.set('8946150','Anvay Joshi');
+map.set('8945584','Aditya kanawade');
+map.set('8945195','Mithali Dhoot');
+map.set('8938978','Prathamesh Rathod');
+map.set('8944072','SHIVAM MISHRA');
+map.set('8943640','Aayush Bhandari');
+map.set('8943105','Purva Khairkar');
+map.set('8943019','Shrey Bhanuse');
+map.set('8942693','Sahil Wankhade');
+map.set('8941925','Bhushan Sonawane');
+map.set('8941899','Samyak Bhandari');
+map.set('8941412','Rushikesh Patil');
+map.set('8941358','Ganesh Lute');
+map.set('8941296','Prasad pawar');
+map.set('8941195','Yogita Bhandkkar');
+map.set('8940266','Atharva Dhumal');
+map.set('8940263','Prathamesh Thorawade');
+map.set('8940257','Omkar Patil');
+map.set('8940215','Tahir Zakir Bagwan');
+map.set('8940144','Aayush Athawale');
+map.set('8939970','Sumit Pandule');
+map.set('8939910','Mayur Padvi');
+map.set('8939911','Amol Chaudhari');
+map.set('8939900','Rohit');
+map.set('8939890','Sumeet Mastud');
+map.set('8939861','Chetan Patil');
+map.set('8939818','Sarthak Bezalwar');
+map.set('8939760','Vivekkumar Valvi');
+map.set('8939691','Atharva Bhide');
+map.set('8939713','Jiten Topiwala');
+map.set('8939657','Sanskriti Omre');
+map.set('8939508','Hrutuja Patare');
+map.set('8939499','Siddhika Khairnar');
+map.set('8939449','Om Shah');
+map.set('8939379','Aishwarya Pillai');
+map.set('8939065','Omkar Janawekar');
+map.set('8938643','Gayatri Patil');
+map.set('8938576','Shreeraj Matsagar');
+map.set('8938147','Ganesh Bhadre');
+map.set('8938094','Vivek Kumbhar');
+map.set('8937898','Parag Ghatol');
+map.set('8937832','Sakshi Avhad');
+map.set('8937758','Parneet Prasad');
+map.set('8937737','Prajwal Shirsath');
+map.set('8937699','Anvesh Mahajan');
+map.set('8937589','Abhinav');
+map.set('8937605','Kanak Meshram');
+map.set('8937566','Piyush Patil');
+map.set('8937487','Advaith Hari');
+map.set('8937416','Aayush Ojha');
+map.set('8937422','Gaurav Sonewane');
+map.set('8937189','Shekhar Gosavi');
+map.set('8937103','Siddhesh Palod');
+map.set('8935171','Aditya Sanju');
+map.set('8934816','Sakshi Pangarkar');
+map.set('8934735','Akshad Burde');
+map.set('8934255','Omkar Bankar');
+map.set('8934225','Amruta Mhetre');
+map.set('8933916','Sumit jadhav');
+map.set('8933117','Hariom Badarke');
+map.set('8932759','Vivek Mahajan');
+map.set('8932490','Shivraj Ghaytidak');
+map.set('8932445','Yashodeep Thanage');
+map.set('8932094','Aayush Desai');
+map.set('8931847','Mandar Warkad');
+map.set('8931661','Amaan Attar');
+map.set('8931623','Tejas Nawale');
+map.set('8931605','Kirti Fatak');
+map.set('8931592','Pratod Daware');
+map.set('8931574','ANJANEYA JOSHI');
+map.set('8931459','Koustub Patil');
+map.set('8931372','Ishita Humnabadkar');
+map.set('8931380','Rushikesh Mohite');
+map.set('8931373','Soham Jane');
+map.set('8931267','Piyush Ladda');
+map.set('8931175','Mohit Panpaliya');
+map.set('8931153','Om Sarda');
+map.set('8931085','Manav Nikam');
+map.set('8931053','Manthan Dumana');
+map.set('8931029','Pruthviraj Katikar');
+map.set('8931005','Tushar Salunkhe');
+map.set('8930772','Sandesh Sahane');
+map.set('8930591','PRAJWAL PADWAL');
+map.set('8930568','Gopal Kumar');
+map.set('8930539','Vinit Patel');
+map.set('8930444','Swayam Katwate');
+map.set('8930308','Paras Bhosale');
+map.set('8930312','Digvijay Patil');
+map.set('8930237','Neha Gawali');
+map.set('8929959','Saurabh Nagare');
+map.set('8929923','Aditya Joshi');
+map.set('8929475','Raj Sakhare');
+map.set('8929447','Anvi Jamkhande');
+map.set('8928891','SUDARSHANI SUMIT');
+map.set('8928520','Debapriya Panchadhyayee');
+map.set('8927288','Srushti Patil');
+map.set('8927212','Shrey Thakur');
+map.set('8927132','Mateen Mohasin Mujawar');
+map.set('8927061','Abhishek Kasamule');
+map.set('8926931','Singh Archita');
+map.set('8926819','Manas Zanwar');
+map.set('8922936','Sanket Meshram');
+map.set('8922894','Aditya Wagatkar');
+map.set('8922781','Om Gurav');
+map.set('8922442','Sharayu Kadam');
+map.set('8922422','Tejas Subhedar');
+map.set('8922374','Aman Patil');
+map.set('8922354','Shivanjali Shinde');
+map.set('8922149','Chirantan Patil');
+map.set('8921938','Dhiraj Murmude');
+map.set('8921594','Raj Rathi');
+map.set('8921859','Viraj Bakshi');
+map.set('8921850','Aditya Sayam');
+map.set('8921837','Atharv Mali');
+map.set('8921814','Harshal Bagul');
+map.set('8921804','Vedant Wadgaonkar');
+map.set('8921658','Krushal Mahajan');
+map.set('8921625','Prathamesh Temburne');
+map.set('8921235','Vinayak Mahajan');
+map.set('8921231','Suyash Khambalkar');
+map.set('8921041','Ajinkya Kadam');
+map.set('8921023','SUTAR SANIYA');
+map.set('8920262','Vishal Bhagwan Sagar');
+map.set('8920242','Prasanna Sunil Batte');
+map.set('8919542','Ameya Gurjar');
+map.set('8919492','Omkar Mandade');
+map.set('8918080','Sanchit Rajmane');
+map.set('8916908','ANMOL BINDROO');
+map.set('8916518','Rushikesh R Netke');
+map.set('8913758','Omkar Oak');
+map.set('8913665','Prathmesh Sadavarte');
+map.set('8913642','Anuj Mohite');
+map.set('8911343','Kushal Baviskar');
+map.set('9834531675','Aniket Lohote');
+map.set('9130065540','Parth Yewale');
+map.set('9146323653','Mukta Hulyalkar');
+
+
 const { PDFDocument, rgb, degrees } = PDFLib;
 
 
@@ -37,7 +177,7 @@ submitBtn.addEventListener("click", () => {
 }
 else
 {
-  alert("Invalid Mail ID");
+  alert("Invalid Regd. ID");
 }
 });
 
@@ -64,11 +204,12 @@ const generatePDF = async (name) => {
 
   // Draw a string of text diagonally across the first page
   firstPage.drawText(name, {
-    x: 350,
-    y: 278,
-    size: 25,
+    x: 275,
+    y: 260,
+    size: 26,
+    // fontWeight = 'bold',
     font: SanChezFont,
-    color: rgb(0.2, 0.84, 0.67),
+    color: rgb(0,0,0),
   });
 
   // Serialize the PDFDocument to bytes (a Uint8Array)
